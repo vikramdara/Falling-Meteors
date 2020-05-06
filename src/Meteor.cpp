9daae6c7ac2d7c8dd635ac1af54079df2c5204ec
vikram dara <vdara2@illinois.edu>
2020-05-06 06:09:49 -0500

    Made switch statement logic for changing starting positions of meteors in wave 4.

--- src/Meteor.cpp
+++ src/Meteor.cpp
@@ -7,14 +7,34 @@
 namespace mylibrary {
 
 Meteor::Meteor(b2World* world, float radius, mylibrary::Wave wave) {
+  cinder::randSeed(std::time(nullptr));
   cinder::vec2 pos = cinder::vec2(cinder::randFloat(200, cinder::app::getWindowWidth() - 200), cinder::randFloat( -40, -100));
   cinder::vec2 posScaled = mylibrary::Conversions::pointsToMeters( pos );
+
   b2BodyDef meteor;
   meteor.type = b2_dynamicBody;
-  meteor.position.Set(posScaled.x, posScaled.y);
-  cinder::randSeed(std::time(nullptr));
+
   const float velMax = 2;
   meteor.linearVelocity = b2Vec2( cinder::randFloat( -velMax, velMax ), cinder::randFloat( -velMax, 0 ) );
+
+  switch (static_cast<int>(std::floor(timer.getSeconds())) / 5) {
+    case 0:
+      pos = cinder::vec2(cinder::randFloat(cinder::app::getWindowWidth() + 40, cinder::app::getWindowWidth() + 80),
+          cinder::randFloat( 0, cinder::app::getWindowHeight() / 2));
+      posScaled = mylibrary::Conversions::pointsToMeters( pos );
+      meteor.linearVelocity = b2Vec2( cinder::randFloat( -100, -30 ), cinder::randFloat( 0, 30 ) );
+      break;
+    case 2:
+      pos = cinder::vec2(cinder::randFloat(-80, -40),
+          cinder::randFloat( 0, cinder::app::getWindowHeight() / 2));
+      posScaled = mylibrary::Conversions::pointsToMeters( pos );
+      meteor.linearVelocity = b2Vec2( cinder::randFloat( 30, 100 ), cinder::randFloat( 0, 30 ) );
+      break;
+  }
+
+
+  meteor.position.Set(posScaled.x, posScaled.y);
+
 
   meteor_body = world->CreateBody(&meteor);
 
